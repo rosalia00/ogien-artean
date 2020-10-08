81e51d112622dfa1dc5c352a9659c56c7c6e6a85
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-08 18:36:36 +0200

    PERSONA Y EMPLEADO

---
+++ OgienArtean/src/Empleado.java
@@ -0,0 +1,41 @@
+
+public class Empleado extends Persona {
+	String tipo;
+	double nomina;
+	
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public double getNomina() {
+		return nomina;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public void setNomina(double nomina) {
+		this.nomina = nomina;
+	}
+	
+	
+	public Empleado(String nombre, String apellido, int dni, String direccion, int telefono, String tipo,
+			double nomina) {
+		super(nombre, apellido, dni, direccion, telefono);
+		this.tipo = tipo;
+		this.nomina = nomina;
+	}
+	
+	public Empleado() {
+		super();
+		this.tipo = "";
+		this.nomina = 0.0;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Empleado [tipo=" + tipo + ", nomina=" + nomina + ", nombre=" + nombre + ", apellido=" + apellido
+				+ ", dni=" + dni + ", direccion=" + direccion + ", telefono=" + telefono + "]";
+	}		
+
+}
---
+++ OgienArtean/src/Persona.java
@@ -0,0 +1,68 @@
+
+public class Persona {
+	String nombre;
+	String apellido;
+	int dni;
+	String direccion;
+	int telefono;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	public Persona(String nombre, String apellido, int dni, String direccion, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	public Persona() {
+		super();
+		this.nombre = "";
+		this.apellido = "";
+		this.dni = 0;
+		this.direccion = "";
+		this.telefono = 0;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", direccion=" + direccion
+				+ ", telefono=" + telefono + "]";
+	}
+	
+}
