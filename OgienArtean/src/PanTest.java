2f050ccc51c3a043e5dccab2fdc6cfdf5a4302d2
Rosalía Pérez <rosalia.perez@opendeusto.es>
2020-10-28 14:43:13 +0100

    Test Pan

--- OgienArtean/src/PanTest.java
+++ OgienArtean/src/PanTest.java
@@ -4,16 +4,67 @@ import org.junit.Test;
 
 public class PanTest {
 	
-
-	//ArrayList<String> ingredientes1 = new ArrayList<String>();
-	//ingredientes1.add("harina");
-	//ingredientes1.add("levadura");
-	//ingredientes1.add("sal");
-	//ingredientes1.add("agua");
+	@Test
+	public void testGetIngredientes() {
+		ArrayList<String> ingredientes = new ArrayList<String>();
+		ingredientes.add("harina");
+		ingredientes.add("levadura");
+		ingredientes.add("sal");
+		ingredientes.add("agua");
+		
+		Pan p = new Pan("Baguette", 0.75, false, ingredientes, true);
+		
+		assertEquals("harina", p.getIngredientes().get(0));
+	}
 	
-	//@Test
-	//public void testGetSal() {
-	//	Pan p = new Pan("Baguette", 0.75, false, <harina,agua, sal, levadura>, );
-	//}
+	@Test
+	public void testSetIngredientes() {
+		ArrayList<String> ingredientes = new ArrayList<String>();
+		ingredientes.add("harina");
+		ingredientes.add("levadura");
+		ingredientes.add("sal");
+		ingredientes.add("agua");
+		
+		Pan p = new Pan("Baguette", 0.75, false, ingredientes, true);
+		Pan p2 = new Pan();
+		p2.setIngredientes(p.getIngredientes());
+		
+		assertEquals("harina", p2.getIngredientes().get(0));
+	}
+	
+
+	@Test
+	public void testGetSal() {
+		ArrayList<String> ingredientes = new ArrayList<String>();
+		ingredientes.add("harina");
+		ingredientes.add("levadura");
+		ingredientes.add("sal");
+		ingredientes.add("agua");
+		
+		Pan p = new Pan("Baguette", 0.75, false, ingredientes, true);
+		
+		assertTrue(p.getSal());
+		
+	}
+	
+	@Test
+	public void testSetSal() {
+		ArrayList<String> ingredientes = new ArrayList<String>();
+		ingredientes.add("harina");
+		ingredientes.add("levadura");
+		ingredientes.add("sal");
+		ingredientes.add("agua");
+		
+		Pan p = new Pan("Baguette", 0.75, false, ingredientes, true);
+		p.setSal(false);
+		
+		assertFalse(p.getSal());
+		
+	}
+	
+	
+	
+	
+	
 
 }
