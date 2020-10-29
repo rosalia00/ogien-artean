00b438d747bb9458f8dd0d277fc70aed8ca6a92f
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2020-10-29 19:18:14 +0100

    Añadir botones prueba 1

--- OgienArtean/src/VentanaClienteInicio.java
+++ OgienArtean/src/VentanaClienteInicio.java
@@ -21,6 +21,17 @@ public class VentanaClienteInicio extends JFrame{
 	//Texto
 	JLabel textoInicio;
 	
+	//JPaneles vacios para el orden del gridlayout
+	JPanel vacio1;
+	JPanel vacio2;
+	JPanel vacio3;
+	JPanel vacio4;
+	JPanel vacio5;
+	JPanel vacio6;
+	JPanel vacio7;
+	JPanel vacio8;
+	JPanel vacio9;
+	
 	public VentanaClienteInicio() {
 		
 		panaderia = new JButton("Panadería");
@@ -36,6 +47,32 @@ public class VentanaClienteInicio extends JFrame{
 		perfil.setBounds(0, 0, 200, 50);
 		
 		textoInicio = new JLabel("Seleccione que clase de producto desea comprar, por favor:");
+		
+		vacio1 = new JPanel();
+		vacio2 = new JPanel();
+		vacio3 = new JPanel();
+		vacio4 = new JPanel();
+		vacio5 = new JPanel();
+		vacio6 = new JPanel();
+		vacio7 = new JPanel();
+		vacio8 = new JPanel();
+		vacio9 = new JPanel();
+		
+		add(vacio1);
+		add(vacio2);
+		add(vacio3);
+		add(miCarro);
+		add(perfil);
+		add(vacio4);
+		add(textoInicio);
+		add(vacio5);
+		add(vacio6);
+		add(vacio7);
+		add(vacio8);
+		add(panaderia);
+		add(pasteleria);
+		add(cocina);
+		add(vacio9);
 		
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setTitle("Cliente Inicio");
