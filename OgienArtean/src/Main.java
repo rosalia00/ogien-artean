b35d039910f842c8861d0230f3638a71e644a33e
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-29 23:49:52 +0100

    VENTANACLIENTECONFIRMACIONCOMPRA funciona (minimos2)

--- OgienArtean/src/Main.java
+++ OgienArtean/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		VentanaClienteInicio a = new VentanaClienteInicio();
+
+		
+		VentanaClienteConfirmacionCompra b = new VentanaClienteConfirmacionCompra();
 	}
 
 }
---
+++ OgienArtean/src/VentanaClienteConfirmacionCompra.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+public class VentanaClienteConfirmacionCompra extends JFrame {
+
+	JButton carro;
+	JButton perfil;
+	JTextPane coso;
+	JButton cancelar;
+	JButton confirmar;
+	JPanel arriba;
+	JPanel centro;
+	JPanel abajo;
+
+	public VentanaClienteConfirmacionCompra() {
+
+		carro = new JButton();
+		perfil = new JButton();
+		coso = new JTextPane();
+		cancelar = new JButton();
+		confirmar = new JButton();
+
+		arriba = new JPanel();
+		centro = new JPanel();
+		abajo = new JPanel();
+
+		arriba.add(carro);
+		arriba.add(perfil);
+		centro.add(coso);
+		abajo.add(cancelar);
+		abajo.add(confirmar);
+
+		add(arriba);
+		add(centro);
+		add(abajo);
+
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Confirmación compra");
+		setSize(600, 300);
+		setLayout(new GridLayout(3, 1, 0, 0));
+
+		setVisible(true);
+
+	}
+
+}
