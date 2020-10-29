56773d2f0e5fa1e0a6d92af6fd24d09a9b76c3eb
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2020-10-29 18:50:13 +0100

    VentanaClienteInicio creación de elementos

---
+++ OgienArtean/src/VentanaClienteInicio.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+import java.awt.event.*;
+
+import java.io.*;
+
+
+import javax.swing.*;
+
+public class VentanaClienteInicio extends JFrame{
+	
+	//Botones de compra
+	JButton panaderia;
+	JButton pasteleria;
+	JButton cocina;
+	
+	//Botones de usuario
+	JButton miCarro;
+	JButton perfil;
+	
+	//Texto
+	JLabel textoInicio;
+}
