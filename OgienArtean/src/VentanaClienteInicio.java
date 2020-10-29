b2c8838bd4ef5369775d54c6493fdc4b50e2fde9
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2020-10-29 19:05:11 +0100

    Cliente inicio definir los limites y el layout de la ventana

--- OgienArtean/src/VentanaClienteInicio.java
+++ OgienArtean/src/VentanaClienteInicio.java
@@ -24,12 +24,24 @@ public class VentanaClienteInicio extends JFrame{
 	public VentanaClienteInicio() {
 		
 		panaderia = new JButton("Panadería");
+		panaderia.setBounds(0, 0, 200, 50);
 		pasteleria = new JButton("Pastelería");
+		pasteleria.setBounds(0, 0, 200, 50);
 		cocina = new JButton("Cocina");
+		cocina.setBounds(0, 0, 200, 50);
 		
 		miCarro = new JButton("Mi Carro");
+		miCarro.setBounds(0, 0, 200, 50);
 		perfil = new JButton("Perfil");
+		perfil.setBounds(0, 0, 200, 50);
 		
 		textoInicio = new JLabel("Seleccione que clase de producto desea comprar por favor:");
+		
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Cliente Inicio");
+		setSize(600, 500);
+		setLayout(new GridLayout(3, 5));
+		setVisible(true);
+		
 	}
 }
