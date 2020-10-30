7295dbe1f8adec4512bc5bad58f654b8e5eaf72f
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-30 21:03:01 +0100

    VENTANA COMPRA ESTETICA VOL1

--- OgienArtean/src/Main.java
+++ OgienArtean/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		VentanaCompra d = new VentanaCompra();
 		//VentanaEmpleadoAdministrar c = new VentanaEmpleadoAdministrar();
 		//VentanaEmpleadoInicio v = new VentanaEmpleadoInicio();
-		VentanaEmpleadoPedidos c = new VentanaEmpleadoPedidos();
+		//VentanaEmpleadoPedidos c = new VentanaEmpleadoPedidos();
 	}
 	
 }
--- OgienArtean/src/VentanaCompra.java
+++ OgienArtean/src/VentanaCompra.java
@@ -1,4 +1,4 @@
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -12,11 +12,16 @@ public class VentanaCompra extends JFrame {
 	JSpinner itemSpinner0;
 
 	JButton comprar;
+	JButton volver;
 
 	JPanel arriba;
-	JPanel centro;
+	JPanel centro0;
 	JPanel centro1;
 	JPanel centro2;
+	JPanel centro00;
+	JPanel centro11;
+	JPanel centro22;
+
 	JPanel abajo;
 
 	public VentanaCompra() {
@@ -25,52 +30,73 @@ public class VentanaCompra extends JFrame {
 		perfil = new JButton("PERFIL");
 
 		comprar = new JButton("COMPRAR");
+		volver =  new JButton("VOLVER");
 
 		arriba = new JPanel();
-		centro = new JPanel();
-		centro.setLayout(new GridLayout(2, 1));
+		centro0 = new JPanel();
+		centro0.setLayout(new GridLayout(1, 6, 10, 60));
 		centro1 = new JPanel();
-		centro1.setLayout(new GridLayout(3, 1));
+		centro1.setLayout(new GridLayout(1, 6, 10, 60));
 		centro2 = new JPanel();
-		centro2.setLayout(new GridLayout(3, 1));
+		centro2.setLayout(new GridLayout(1, 6, 10, 60));
+
+		centro00 = new JPanel();
+		centro00.setLayout(new GridLayout(1, 6, 10, 5));
+		centro11 = new JPanel();
+		centro11.setLayout(new GridLayout(1, 6, 10, 5));
+		centro22 = new JPanel();
+		centro22.setLayout(new GridLayout(1, 6, 10, 5));
 		abajo = new JPanel();
 
 		arriba.add(carro);
 		arriba.add(perfil);
 
-		for (int i = 0; i <= 5; i++) {
+		for (int i = 1; i <= 5; i++) {
 			item0 = new JButton("COSO");
+			item0.setOpaque(false);
+			item0.setContentAreaFilled(false);
+			item0.setBorderPainted(false);
+			item0.setFocusPainted(false);
 			itemLabel0 = new JLabel("NOMBRE COSO");
-			itemSpinner0 = new JSpinner();
+			itemSpinner0 = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
 
-			centro1.add(item0);
+			centro0.add(item0);
 			centro1.add(itemLabel0);
-			centro1.add(itemSpinner0);
-		}
-
-		for (int i = 0; i <= 5; i++) {
-			item0 = new JButton("COSO");
-			itemLabel0 = new JLabel("NOMBRE COSO");
-			itemSpinner0 = new JSpinner();
-
-			centro2.add(item0);
-			centro2.add(itemLabel0);
 			centro2.add(itemSpinner0);
 		}
 
-		centro.add(centro1);
-		centro.add(centro2);
+		for (int i = 1; i <= 5; i++) {
+			item0 = new JButton("COSO");
+			item0.setOpaque(false);
+			item0.setContentAreaFilled(false);
+			item0.setBorderPainted(false);
+			item0.setFocusPainted(false);
+			itemLabel0 = new JLabel("NOMBRE COSO");
+			itemSpinner0 = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
 
+			centro00.add(item0);
+			centro11.add(itemLabel0);
+			centro22.add(itemSpinner0);
+		}
+		
+		abajo.add(volver);
 		abajo.add(comprar);
 
 		add(arriba);
-		add(centro);
+		add(centro0);
+		add(centro1);
+		add(centro2);
+		add(centro00);
+		add(centro11);
+		add(centro22);
 		add(abajo);
+
+		setIconImage(Toolkit.getDefaultToolkit().getImage("octocat.png"));
 
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setTitle("COMPRA");
 		setSize(600, 300);
-		setLayout(new GridLayout(3, 1));
+		setLayout(new GridLayout(8, 1));
 		setVisible(true);
 
 	}
