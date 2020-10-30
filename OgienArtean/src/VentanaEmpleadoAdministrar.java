c7e32940c9fd68fd9351ef85d9ab9f87e7c1b058
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2020-10-30 15:48:43 +0100

    VentanaEmpleadoAdministrar prueba 2

--- OgienArtean/src/VentanaEmpleadoAdministrar.java
+++ OgienArtean/src/VentanaEmpleadoAdministrar.java
@@ -29,23 +29,20 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 		horas = new JButton("Horas");
 		perfil = new JButton("Perfil");
 		
+		empleados = new JPanel();
+		
 		for (int i = 0; i < 10; i++) {
 			empleadoPerfil = new JPanel();
-			empleadoPerfil.setLayout(new GridLayout(3, 1));
 			
 			fotoEmpleado = new JPanel();
 			nombreYApellidos = new JLabel("BD");
 			opcion = new JComboBox();
 			
-			opcion.add(eliminar = new JLabel("Eliminar"));
-			opcion.add(añadir = new JLabel("Añadir"));
-			
 			empleadoPerfil.add(fotoEmpleado);
 			empleadoPerfil.add(nombreYApellidos);
 			empleadoPerfil.add(opcion);	
 			
-			empleados = new JPanel();
-			empleados.setLayout(new GridLayout(2, 5, 10, 10));
+			empleados.setLayout(new GridLayout(2, 5));
 			empleados.add(empleadoPerfil);
 		} 
 		
