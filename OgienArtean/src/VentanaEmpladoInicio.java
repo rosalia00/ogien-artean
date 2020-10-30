55b4c0653f97c5854a373e733c30fbec8ec4da9c
Rosalía Pérez <rosalia.perez@opendeusto.es>
2020-10-30 15:56:08 +0100

    VentanaEmpleadoInicio creacion

---
+++ OgienArtean/src/VentanaEmpladoInicio.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class VentanaEmpladoInicio extends JFrame {
+	//Botones opciones de empleado
+	JButton administrar;
+	JButton pedidos;
+	JButton registro;
+	
+	//Botones de perfil de empleado
+	JButton horas;
+	JButton perfil;
+	
+	//Paneles
+	JPanel arriba;
+	JPanel abajo;
+	
+	
+	
+}
