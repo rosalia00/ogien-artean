5ed8749f97da5ebb4bd1becd1326cd0f4a4885a1
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-31 17:14:40 +0100

    VENTANAMICARRO - NOMBRE

--- OgienArtean/src/VentanaMiCarro.java
+++ OgienArtean/src/VentanaMiCarro.java
@@ -1,35 +1,61 @@
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class VentanaMiCarro extends JFrame {
 	
-	JPanel volver;
-	JPanel nombre;
+	JButton volver;
+	JButton nombre;
 	JTextPane coso;
 	JButton comprar;
 	
-	JPanel arriba;
+	JPanel abajo;
 	
 	public VentanaMiCarro() {
 		
-		volver  = new JPanel();
-		nombre = new JPanel();
-		coso = new JTextPane();
-		comprar = new JButton();
-		arriba = new JPanel();
+		nombre = new JButton();
+		nombre.setIcon(new ImageIcon("carro.png"));
+		nombre.setContentAreaFilled(false);
+		nombre.setBorderPainted(false);
+		nombre.setFocusPainted(false);
 		
-		arriba.add(volver);
-		arriba.add(nombre);
-		add(arriba);
+		coso = new JTextPane();
+		
+		volver  = new JButton();
+		
+		comprar = new JButton();
+		comprar.setIcon(new ImageIcon("comprar.png"));
+		comprar.setContentAreaFilled(false);
+		comprar.setBorderPainted(false);
+		comprar.setFocusPainted(false);
+		comprar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new VentanaClienteConfirmacionCompra();
+				dispose();
+			}
+		});
+		
+		abajo = new JPanel();
+		
+		setContentPane(new JLabel(new ImageIcon("fondo3.png")));
+		
+		abajo.add(volver);
+		abajo.add(comprar);
+		add(nombre);
 		add(coso);
-		add(comprar);
+		add(abajo);
+		
+		setIconImage(Toolkit.getDefaultToolkit().getImage("octocat.png"));
 		
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setTitle("Mi Perfil");
+		setTitle("MI CARRO");
 		setSize(550, 550);
 		setLayout(new GridLayout(3, 1));
 		setVisible(true);
+		setResizable(false);
 		
 	}
 	
