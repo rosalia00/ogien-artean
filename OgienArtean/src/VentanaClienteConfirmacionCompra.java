1c9adec1919e70a7c4ca8a84a1b86a05420299d7
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-31 12:21:03 +0100

    VENATANCLIENTECONFIRMACION - ESTETICO HECHO + EXTRA IMAGENES

--- OgienArtean/src/VentanaClienteConfirmacionCompra.java
+++ OgienArtean/src/VentanaClienteConfirmacionCompra.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -15,31 +17,79 @@ public class VentanaClienteConfirmacionCompra extends JFrame {
 
 	public VentanaClienteConfirmacionCompra() {
 
-		carro = new JButton("CARRO");
-		perfil = new JButton("PERFIL");
+		carro = new JButton();
+		carro.setIcon(new ImageIcon("carro.png"));
+		carro.setContentAreaFilled(false);
+		carro.setBorderPainted(false);
+		carro.setFocusPainted(false);
+		
+		perfil = new JButton();
+		perfil.setIcon(new ImageIcon("perfil.png"));
+		perfil.setContentAreaFilled(false);
+		perfil.setBorderPainted(false);
+		perfil.setFocusPainted(false);
+		perfil.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new VentanaPerfil();
+				dispose();
+			}
+		});
+		
 		coso = new JTextPane();
-		cancelar = new JButton("CANCELAR");
-		confirmar = new JButton("CONFIRMAR");
+		cancelar = new JButton();
+		cancelar.setIcon(new ImageIcon("cancelar.png"));
+		cancelar.setContentAreaFilled(false);
+		cancelar.setBorderPainted(false);
+		cancelar.setFocusPainted(false);
+		cancelar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		
+		confirmar = new JButton();
+		confirmar.setIcon(new ImageIcon("confirmar.png"));
+		confirmar.setContentAreaFilled(false);
+		confirmar.setBorderPainted(false);
+		confirmar.setFocusPainted(false);
+		confirmar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		
 
 		arriba = new JPanel();
+		arriba.setOpaque(false);
 		centro = new JPanel();
+		centro.setOpaque(false);
 		abajo = new JPanel();
+		abajo.setOpaque(false);
 
 		arriba.add(carro);
 		arriba.add(perfil);
 		centro.add(coso);
 		abajo.add(cancelar);
 		abajo.add(confirmar);
+		
+		setContentPane(new JLabel(new ImageIcon("fondo3.png")));
 
 		add(arriba);
 		add(centro);
 		add(abajo);
+		
+		setIconImage(Toolkit.getDefaultToolkit().getImage("octocat.png"));
 
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setTitle("Confirmación compra");
-		setSize(600, 300);
+		setSize(1500, 800);
 		setLayout(new GridLayout(3, 1, 0, 0));
-
+		setResizable(false);
 		setVisible(true);
 
 	}
