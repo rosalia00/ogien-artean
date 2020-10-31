ef4b09fc07e9a446c122e3745f6e6b91c7e56b13
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-31 17:05:07 +0100

    CREACION Y PRUEBAS

--- OgienArtean/src/Main.java
+++ OgienArtean/src/Main.java
@@ -3,10 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		VentanaMiCarro f = new VentanaMiCarro();
 		//VentanaCompra d = new VentanaCompra();
 		//VentanaClienteConfirmacionCompra z = new VentanaClienteConfirmacionCompra();
-		VentanaEmpleadoAdministrar c = new VentanaEmpleadoAdministrar();
+		//VentanaEmpleadoAdministrar c = new VentanaEmpleadoAdministrar();
 		//VentanaEmpleadoInicio v = new VentanaEmpleadoInicio();
 		//VentanaEmpleadoPedidos c = new VentanaEmpleadoPedidos();
 		//VentanaClienteInicio a = new VentanaClienteInicio();
--- OgienArtean/src/VentanaMiCarro.java
+++ OgienArtean/src/VentanaMiCarro.java
@@ -1,17 +1,35 @@
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class VentanaMiCarro extends JFrame {
 	
+	JPanel volver;
 	JPanel nombre;
 	JTextPane coso;
 	JButton comprar;
 	
+	JPanel arriba;
+	
 	public VentanaMiCarro() {
 		
+		volver  = new JPanel();
 		nombre = new JPanel();
 		coso = new JTextPane();
 		comprar = new JButton();
+		arriba = new JPanel();
 		
+		arriba.add(volver);
+		arriba.add(nombre);
+		add(arriba);
+		add(coso);
+		add(comprar);
+		
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("Mi Perfil");
+		setSize(550, 550);
+		setLayout(new GridLayout(3, 1));
+		setVisible(true);
 		
 	}
 	
