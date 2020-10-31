3535369af1bc57867b077a23386404961a088f9f
Tyler de Mier <tylerdemier@opendeusto.es>
2020-10-31 17:00:00 +0100

    MICARRO ESQUELETO

---
+++ OgienArtean/src/VentanaMiCarro.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class VentanaMiCarro extends JFrame {
+	
+	JPanel nombre;
+	JTextPane coso;
+	JButton comprar;
+	
+	public VentanaMiCarro() {
+		
+		nombre = new JPanel();
+		coso = new JTextPane();
+		comprar = new JButton();
+		
+		
+	}
+	
+	
+}
