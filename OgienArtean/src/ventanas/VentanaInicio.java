62a70b5dc98118bddca9a82252917154081034a7
Mikel del Hoyo <mikel.delhoyo@opendeusto.es>
2020-11-13 17:58:15 +0100

    VentanaIncio crear botones

---
+++ OgienArtean/src/ventanas/VentanaInicio.java
@@ -0,0 +1,20 @@
+package ventanas;
+
+import java.awt.*;
+
+import java.awt.event.*;
+
+import java.io.*;
+
+import javax.swing.*;
+
+public class VentanaInicio extends JFrame{
+	
+	JButton Cliente;
+	JButton Empleado;
+	
+	public VentanaInicio() {
+		
+	}
+
+}
