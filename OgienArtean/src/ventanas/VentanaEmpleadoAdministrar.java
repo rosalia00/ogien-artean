8576083fd782ad48bddfd36a0d3cbfc0a8932e27
Tyler de Mier <tylerdemier@opendeusto.es>
2020-12-06 18:30:47 +0100

    botones de eleccion

--- OgienArtean/src/ventanas/VentanaEmpleadoAdministrar.java
+++ OgienArtean/src/ventanas/VentanaEmpleadoAdministrar.java
@@ -1,4 +1,5 @@
 package ventanas;
+
 import java.awt.*;
 import java.util.logging.Logger;
 import java.util.logging.LogManager;
@@ -10,30 +11,17 @@ import java.io.*;
 
 import javax.swing.*;
 
-public class VentanaEmpleadoAdministrar extends JFrame{
-	
-	JTextField nombre;
-	JSpinner precio;
-	JTextField ingredientes;
-	JButton añadirIngredientes;
-	JRadioButton celiaco;
-	JLabel celiacoNombre;
-	JRadioButton sal;
-	JLabel salNombre;
-	
+public class VentanaEmpleadoAdministrar extends JFrame {
+
 	JButton perfil;
-	
-	JPanel panPoner;
-	
+
 	JPanel botones;
-	JPanel empleadoPerfil;
-	JPanel empleados;
-	
+
 	JButton aceptar;
-	
+
 	JLabel eliminar;
 	JLabel añadir;
-	
+
 	JTextField cNombre;
 	JLabel cNombreLabel;
 	JSpinner cPrecio;
@@ -47,7 +35,7 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 	JButton cAñadir;
 	JPanel cPanelArriba;
 	JPanel cPanel;
-	
+
 	JTextField bNombre;
 	JLabel bNombreLabel;
 	JSpinner bPrecio;
@@ -63,7 +51,7 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 	JButton bAñadir;
 	JPanel bPanelArriba;
 	JPanel bPanel;
-	
+
 	JTextField pNombre;
 	JLabel pNombreLabel;
 	JSpinner pPrecio;
@@ -75,36 +63,33 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 	JButton pAñadir;
 	JPanel pPanelArriba;
 	JPanel pPanel;
-	
-	
-	
-	
+
+	private int numero = 1;
+	JButton izquierda;
+	JLabel opcion;
+	JButton derecha;
+	JPanel derechaAbajo;
 
 	private static Logger logger = Logger.getLogger(VentanaEmpleadoAdministrar.class.getName());
-	
+
 	public VentanaEmpleadoAdministrar() {
-		
-		
+
 		perfil = new JButton();
 		perfil.setContentAreaFilled(false);
 		perfil.setBorderPainted(false);
 		perfil.setFocusPainted(false);
 		perfil.setIcon(new ImageIcon("imagenes/perfil.png"));
 		perfil.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				VentanaPerfil a = new VentanaPerfil();
 				dispose();
 				logger.log(Level.INFO, "Ha funcionado el boton perfil.");
 			}
 		});
-		
-		empleados = new JPanel();
-		empleados.setOpaque(false);
-		empleados.setLayout(new GridLayout(2, 5));
-		
+
 		cNombre = new JTextField();
 		cNombreLabel = new JLabel("NOMBRE: ");
 		cPrecio = new JSpinner(new SpinnerNumberModel(0, 0.00, 99.99, 0.01));
@@ -135,8 +120,7 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 		cPanelArriba.add(cTipo);
 		cPanel.add(cPanelArriba);
 		cPanel.add(cAñadir);
-		
-		
+
 		bNombre = new JTextField();
 		bNombreLabel = new JLabel("NOMBRE: ");
 		bPrecio = new JSpinner(new SpinnerNumberModel(0, 0.00, 99.99, 0.01));
@@ -166,7 +150,7 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 		bPanelArriba.add(bSal);
 		bPanel.add(bPanelArriba);
 		bPanel.add(bAñadir);
-		
+
 		pNombre = new JTextField();
 		pNombreLabel = new JLabel("NOMBRE: ");
 		pPrecio = new JSpinner(new SpinnerNumberModel(0, 0.00, 99.99, 0.01));
@@ -193,60 +177,67 @@ public class VentanaEmpleadoAdministrar extends JFrame{
 		pPanelArriba.add(pTipo);
 		pPanel.add(pPanelArriba);
 		pPanel.add(pAñadir);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/* for (int i = 0; i < 8; i++) {
-			panPoner = new JPanel();
-			panPoner.setLayout(new GridLayout(5, 2));
-			nombre = new JTextField();
-			precio = new JSpinner();
-			ingredientes = new JTextField();
-			añadirIngredientes = new JButton("AÑADIR");
-			celiaco = new JRadioButton();
-			celiacoNombre = new JLabel("CELIACO");
-			sal = new JRadioButton();
-			salNombre = new JLabel("SAL");
-			
-			panPoner.add(nombre);
-			panPoner.add(precio);
-			panPoner.add(ingredientes);
-			panPoner.add(añadirIngredientes);
-			panPoner.add(celiaco);
-			panPoner.add(celiacoNombre);
-			panPoner.add(sal);
-			panPoner.add(salNombre);
-			
-			empleados.add(panPoner);
-			
-		} */
+
+		izquierda = new JButton("<");
+		opcion = new JLabel("ELECCION");
+		derecha = new JButton(">");
+
+		izquierda.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (numero == 1) {
+					opcion.setText("PAN");
+				} else if (numero == 2) {
+					opcion.setText("PASTELERIA");
+				} else if (numero == 0) {
+					opcion.setText("COMIDA");
+				}
+
+				if (numero <= 2 && numero >= 1) {
+					numero--;
+				} else if (numero == 0) {
+					numero = 2;
+				}
+
+			}
+		});
+
+		derecha.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (numero == 1) {
+					opcion.setText("PAN");
+				} else if (numero == 2) {
+					opcion.setText("PASTELERIA");
+				} else if (numero == 0) {
+					opcion.setText("COMIDA");
+				}
+
+				if (numero < 2 && numero >= 0) {
+					numero++;
+				} else if (numero == 2) {
+					numero = 0;
+				}
+
+			}
+		});
+
+		derechaAbajo = new JPanel();
+		derechaAbajo.add(izquierda);
+		derechaAbajo.add(opcion);
+		derechaAbajo.add(derecha);
+
 		setContentPane(new JLabel(new ImageIcon("imagenes/fondo3.png")));
-		
+
 		botones = new JPanel(new FlowLayout());
 		botones.add(perfil);
 		botones.setOpaque(false);
-		
+
 		add(botones);
-		add(pPanel);
-		
+		add(derechaAbajo);
+
 		setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/octocat1.png"));
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setTitle("ADMINISTRACION");
